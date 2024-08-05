adffee6c66937896c16af9bed0942b2a0727ce43
Manavkotecha <146665096+Manavkotecha@users.noreply.github.com>
2024-08-05 23:37:52 +0530

    Update Lab7_41.java

--- Lab 7/Lab7_41.java
+++ Lab 7/Lab7_41.java
@@ -1,77 +1,41 @@
+
 import java.util.Scanner;
+import java.util.Stack;
 
+public class BalancedParentheses {
 
-class Stack {
-    int top = -1;
-    int size = 0;
-    char[] stack;
-    
-    public Stack(int size) {
-        this.size = size;
-        stack = new char[size];
-    }
-
-    public void push(char ch) {
-        if(top >= size-1) {
-            System.out.println("Stack Overflow");
-            return;
-        }
-        top++;
-        stack[top] = ch;
-    }
-
-    public char pop() {
-        if(top<0) {
-            System.out.println("Stack Underflow");
-            return 0;
-        }
-        top--;
-        return stack[top+1];
-    }
-}
-
-public class Lab7_41 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Enter the number of test cases: ");
-        int t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
-            System.out.println("Enter a string: ");
-            String str = scanner.next();
-            if(str.length()%2 == 1) {
-                System.out.println(0);
-            }
-            else {
-                Stack stack = new Stack(str.length());
-                boolean flag = true;
-                for(int j=0; j<str.length(); j++) {
-                    if(str.charAt(j) == '(') {
-                        stack.push(')');
-                    }
-                    else if(str.charAt(j) == '{') {
-                        stack.push('}');
-                    }
-                    else if(str.charAt(j) == '[') {
-                        stack.push(']');
-                    }
-                    else {
-                        if(stack.pop() != str.charAt(j)) {
-                            flag = false;
-                            break;
-                        }
-                    }
+
+        System.out.println("Enter a string to check for balanced parentheses:");
+        String s = sc.nextLine();
+
+        if (isBalanced(s)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        sc.close();
+    }
+
+    public static boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
                 }
-                if(flag) {
-                    System.out.println(1);
-                }
-                else {
-                    System.out.println(0);
+                char temp = stack.pop();
+                if ((ch == ')' && temp != '(') || (ch == '}' && temp != '{') || (ch == ']' && temp != '[')) {
+                    return false;
                 }
             }
         }
-        sc.close();
-        scanner.close();
+
+        return stack.isEmpty();
     }
 }
