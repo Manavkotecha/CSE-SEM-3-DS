188924154728356306e51f4cee12b0febf89fae7
Manavkotecha <146665096+Manavkotecha@users.noreply.github.com>
2024-08-05 23:35:44 +0530

    Update Lab7_38.java

--- Lab 7/Lab7_38.java
+++ Lab 7/Lab7_38.java
@@ -1,106 +1,75 @@
-import java.util.Scanner;
 
-class Stack {
-    int top = -1;
-    int size = 0;
-    int[] stack;
-    
-    public Stack(int size) {
-        this.size = size;
-        stack = new int[size];
-    }
+public class StackLab {
 
-    public void push(int num) {
-        if(top >= size-1) {
+    public static int push(int s[], int top, int x) {
+        if (top >= s.length - 1) {
             System.out.println("Stack Overflow");
-            return;
+            return top;
+        } else {
+            top += 1;
+            s[top] = x;
+            return top;
         }
-        top++;
-        stack[top] = num;
-        System.out.println("Element inserted succesfully");
-        display();
     }
 
-    public int pop() {
-        if(top<0) {
+    public static int pop(int s[], int top) {
+        if (top < 0) {
             System.out.println("Stack Underflow");
-            return 0;
-        }
-        top--;
-        display();
-        return stack[top+1];
-    }
-
-    public int peep(int i) {
-        if(top-i+1 < 0) {
-            System.out.println("Stack Underflow");
-            return 0;
-        }
-        display();
-        return stack[top-i+1];
-    }
-
-    public void change(int num, int i) {
-        if(top-i+1 < 0) {
-            System.out.println("Stack Underflow");
-            return;
-        }
-        stack[top-i+1] = num;
-        System.out.println("Element changed succesfully");
-        display();
-    }
-
-    public void display() {
-        for(int i=top; i>=0; i--) {
-            System.out.println("stack["+i+"] = "+stack[i]);
+            return top;
+        } else {
+            System.out.println("Popped element: " + s[top]);
+            top -= 1;
+            return top;
         }
     }
-}
 
-public class Lab7_38 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the stack: ");
-        int size = sc.nextInt();
-        Stack stack = new Stack(size);
-        int choice;
-        do{
-            System.out.println();
-            System.out.println("Enter 1 to insert an element\n2 to delete an element from the top\n3 to view an element at any position\nEnter 4 to change element from any position\nEnter 5 to display the stack\nEnter -1 to terminate the menu: ");
-            choice = sc.nextInt();
-            switch(choice) {
-                case 1:
-                    System.out.println("Enter an element to insert: ");
-                    int num = sc.nextInt();
-                    stack.push(num);
-                    break;
+    public static void update(int s[], int top, int pos, int x) {
+        if (pos <= top && pos >= 0) {
+            s[pos] = x;
+            System.out.println("Updated element at position " + pos + " to " + x);
+        } else {
+            System.out.println("Invalid position");
+        }
+    }
 
-                case 2:
-                    System.out.println("Deleted Element is "+stack.pop());
-                    break;
+    public static void peep(int s[], int top, int pos) {
+        if (pos <= top && pos >= 0) {
+            System.out.println("Element at position " + pos + ": " + s[pos]);
+        } else {
+            System.out.println("Invalid position");
+        }
+    }
 
-                case 3:
-                    System.out.println("Enter the position from top to view the element: ");
-                    int i = sc.nextInt();
-                    System.out.println("Element at position "+i+" is "+stack.peep(i));
-                    break;
-
-                case 4:
-                    System.out.println("Enter the position to change the element: ");
-                    int pos = sc.nextInt();
-                    System.out.println("Enter an element to update at possition "+pos+": ");
-                    int n = sc.nextInt();
-                    stack.change(n, pos);
-                    break;
-
-                case 5:
-                    stack.display();
-                    break;
-
-                default:
-                    System.out.println("Invalid Input");
+    public static void display(int s[], int top) {
+        if (top < 0) {
+            System.out.println("Stack is empty");
+        } else {
+            System.out.print("Stack elements: ");
+            for (int i = 0; i <= top; i++) {
+                System.out.print(s[i] + " ");
             }
-        } while(choice != -1);
-        sc.close();
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int s[] = new int[100];
+        int top = -1;
+
+        top = push(s, top, 10);
+        top = push(s, top, 20);
+        top = push(s, top, 30);
+        top = push(s, top, 40);
+        top = push(s, top, 50);
+
+        display(s, top);
+
+        top = pop(s, top);
+
+        update(s, top, 1, 25);
+        display(s, top);
+
+        peep(s, top, 0);
+
     }
 }
