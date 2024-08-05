70fdbe3287d3c33dcb64f5d825f385a219e4b91b
Manavkotecha <146665096+Manavkotecha@users.noreply.github.com>
2024-08-05 23:41:21 +0530

    Update Lab8_47.java

--- Lab 8/Lab8_47.java
+++ Lab 8/Lab8_47.java
@@ -1,77 +1,46 @@
-import java.util.*;
+//546+*493/+* =350
 
-public class Lab8_47 {
+import java.util.Scanner;
+import java.util.Stack;
+
+public class EvaluationOfPostfix {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter postfix expression");
-        String postfix = sc.nextLine();
-        double ans = evalpostfix(postfix);
-        System.out.println("Result: "+ans);
-        sc.close();
-    }
+        Stack<Integer> st = new Stack<>();
 
-    public static double evalpostfix(String postfix) {
-        Stack<Double> stack = new Stack<Double>();
-        String[] arr = postfix.split(",");
-        for(int i=0; i<arr.length; i++) {
-            String temp = arr[i];
-            temp = temp.trim();
-            if(isOperator(temp)) {
-                double opd1, opd2;
-                if(!stack.isEmpty()) opd2 = stack.pop();
-                else {
-                    System.out.println("Invalid postfix expression");
-                    return 0;
-                }
-                if(!stack.isEmpty()) opd1 = stack.pop();
-                else {
-                    System.out.println("Invalid postfix expression");
-                    return 0;
-                }
+        System.out.println("Enter postfix expression:");
+        String s = sc.nextLine();
 
-                switch (temp) {
-                    case "+":
-                        stack.push(opd1+opd2);
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                st.push(ch - '0');
+            } else {
+                int oprator2 = st.pop();
+                int oprator1 = st.pop();
+
+                switch (ch) {
+                    case '+':
+                        st.push(oprator1 + oprator2);
                         break;
-
-                    case "-":
-                        stack.push(opd1-opd2);
+                    case '-':
+                        st.push(oprator1 - oprator2);
                         break;
-
-                    case "*":
-                        stack.push(opd1*opd2);
+                    case '*':
+                        st.push(oprator1 * oprator2);
                         break;
-
-                    case "/":
-                        stack.push(opd1/opd2);
+                    case '/':
+                        st.push(oprator1 / oprator2);
                         break;
-
-                    case "^":
-                        stack.push(Math.pow(opd1, opd2));
-                        break;
+                    default:
+                        System.out.println("Invalid character is there: " + ch);
+                        return;
                 }
             }
-            else {
-                for(int j=0; j<temp.length(); j++) {
-                    if(!Character.isDigit(temp.charAt(j))) {
-                        System.out.println("Invalid postfix expression");
-                        return 0;
-                    }
-                }
-                stack.push(Double.parseDouble(temp));
-            }
         }
-        double ans = stack.pop();
-        if(!stack.isEmpty()) {
-            System.out.println("Invalid postfix expression");
-            return 0;
-        }
-        return ans;
+        System.out.println("Result: " + st.pop());
     }
 
-    public static boolean isOperator(String temp) {
-        if(temp.length() != 1) return false;
-        if(temp.equals("+") || temp.equals("-") || temp.equals("*") || temp.equals("/") || temp.equals("^")) return true;
-        return false;
-    }
 }
